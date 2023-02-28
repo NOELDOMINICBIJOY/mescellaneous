46afed5184a27625d973dcabcfe00929b483aee0
kalauesXXX <noeldominic081@gmail.com>
2023-02-28 22:43:08 +0530

    organised all prereqs of biot-savart into modules

--- biot_savart/pre_reqs/prereqs.py
+++ biot_savart/pre_reqs/prereqs.py
@@ -1,64 +1,65 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-import mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D
+
 
 class Curve:
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, parametarising_variable):
+        self.t = parametarising_variable
         self.x = x
         self.y = y
         self.z = z
-
+        
         curve = []
         index = 0
-    
+
         while index < x.shape[0]:
-            curve.append([x[index], y[index], z[index]])
+            curve.append([self.x[index],self.y[index], self.z[index]])
             index += 1
-    
-        self.curve =  np.array(curve)
 
-    def tangent_to_curve(curve, parametarising_variable):
-        delta_t = parametarising_variable[1] - parametarising_variable[0]
+        self.curve = np.array(curve)
+
+    def tangent_to_curve(self):
+        delta_t = self.t[1] - self.t[0]
         tangents = []
-    
-        for index, element in enumerate(curve):       
-            if index < curve.shape[0] - 1 :
-                delta_s = curve[index + 1] - curve[index]
-                tangents.append(delta_s)           
-        
+
+        for index, element in enumerate(self.curve):
+            if index < self.curve.shape[0] - 1:
+                delta_s = self.curve[index + 1] - self.curve[index]
+                tangents.append(delta_s)
+
         self.tangents = np.array(tangents)
+        return self.tangents
 
-    def get_curve(self):
-        return self.curve
+    def plot_curve(self, figsize = (10, 10), color = '#51ff00', show_plot = False):
+        fig = plt.figure(figsize = figsize)
+        ax = plt.axes(projection = '3d')
+        
+        ax.plot3D(self.x, self.y, self.z, color = color)
+        if show_plot:
+            plt.show
 
-    def get_tangents(self):
-        return self.get_tangents
+    def plot_tangent(self, x, y, z, parametarising_variable_value, figsize, color_of_curve, color_of_tangent, x_lim, y_lim, z_lim, show_plot = False, scaling_factor = 10):
+        """parametarising_variable_value to be between defining bounds of parametarising_variable"""
+        """x, y, z are function unlike the arrays when used in defining the curve"""
 
-    def plot_curve(self, color = 'blue'):
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.plot3D(self.x, self.y, self.z, color=color)
+        t_0 = self.t[parametarising_variable_value]
 
-    def plot_tangent(self, parametarising_variable_value, color):
-        point = parametarising_variable_value
+        fig = plt.figure(figsize = figsize)
+        ax = plt.axes(projection = '3d')
+        
+        ax.plot3D(self.x, self.y, self.z, color = color_of_curve)
 
-        fig = plt.figure(figsize=(10, 10))
-        ax = plt.axes(projection='3d')
-        ax.plot3D(self.x, self.y, self.z, color=color)
-    
-        t_0 = t[point]
-        origin_and_tangent = [x(t_0), y(t_0), z(t_0), 10*tangents[point][0], 10*tangents[point][1], 10*tangents[point][2]]
-
-        soa = np.array([origin_and_tangent])
+        origins_and_tangents = [x(t_0), y(t_0), z(t_0),scaling_factor*self.tangents[parametarising_variable_value][0], scaling_factor*self.tangents[parametarising_variable_value][1], scaling_factor*self.tangents[parametarising_variable_value][2]]
+        soa = np.array([origins_and_tangents])
         X, Y, Z, U, V, W = zip(*soa)
+        
+        ax.quiver(X, Y, Z, U, V, W, color = color_of_tangent)
+        ax.plot3D(x(t_0), y(t_0), z(t_0), 'o', color = color_of_tangent)
 
-        ax.quiver(X, Y, Z, U, V, W, color='cyan')
-        ax.plot3D(x(t_0), y(t_0), z(t_0), 'o', color='cyan')
-
-        ax.set_xlim([-1, 0.5])
-        ax.set_ylim([-1, 1.5])
-        ax.set_zlim([-1, 8])
-        plt.show()
-
-
-
+        ax.set_xlim(x_lim)
+        ax.set_ylim(y_lim)
+        ax.set_zlim(z_lim)
+    
+        if show_plot:
+            plt.show
